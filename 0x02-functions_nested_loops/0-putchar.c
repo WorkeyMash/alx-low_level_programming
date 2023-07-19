c73b8279fcac30585f9c60fc646550199fda84ae
WorkeyMash <mashabelaworkers@gmail.com>
2023-07-19 00:47:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar followed by a newline
+ * main - Entry point of my code,
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
