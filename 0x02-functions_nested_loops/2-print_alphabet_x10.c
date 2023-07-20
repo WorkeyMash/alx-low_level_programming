a983f04267b2a2d62aa1ae1842cf3e74c107f006
WorkeyMash <mashabelaworkers@gmail.com>
2023-07-19 17:48:05 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,11 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char c;
-	i = 0;
+	char letter;
 
 	while (count++ <= 9)
-	{
 
-		for (c = 'a'; c <= 'z'; c++)
-			 _putchar(c);
-		 _putchar('\n');
-		i++;
-	}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 }
